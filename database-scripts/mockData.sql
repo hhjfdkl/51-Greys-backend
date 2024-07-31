50418f5e2f7592c9dea89e1c846c4594291c1fa4
hhjfdkl <nate.koroman@gmail.com>
2024-07-31 08:50:58 -0400

    added some mock data in project and employee tables

---
+++ database-scripts/mockData.sql
@@ -0,0 +1,23 @@
+INSERT INTO employee (first_name, last_name, email, phone_number, occupation, clearance, img)
+VALUES 
+  ('Nate','Koroman','nk@fedmail.net','111-111-1111','Fry Cook','Very Low',null)
+, ('Bob','Dole','bdole@51test.gov','735-894-4900','Administration and Management','High','https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Ks_1996_dole.jpg/480px-Ks_1996_dole.jpg')
+, ('Jim','Smith','jsmith@gmail.org','222-222-2222','Security Guard','Medium',null)
+, ('Serena','Williams','serwil@earthlink.gov','833-671-1781','Science and Research','High','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfjb2I0jARffSB43NMK7HC4IqVwCGC7e-fw&s')
+, ('Kelsey','Williams','kw@yahoo.net','333-333-3333','IT','Medium','https://www.shutterstock.com/shutterstock/photos/1865153395/display_1500/stock-photo-portrait-of-young-smiling-woman-looking-at-camera-with-crossed-arms-happy-girl-standing-in-1865153395.jpg')
+, ('Lindsay','Lohan','llhan@gmail.gov','645-930-6207','Science and Research','High','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Lindsay_Lohan_in_2023_%28cropped%29.jpg/495px-Lindsay_Lohan_in_2023_%28cropped%29.jpg')
+, ('Shawn','Stevens','sste@yahoo.net','444-444-4444','Janitor','Low','https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=')
+, ('Billy','Mayes','mayes@sec.gov','859-876-7250','Security and Compliance','High','https://people.com/thmb/XUoRFER8lu0CDhbFFtT0dfbC5Oc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(119x0:121x2)/billy-mays-320-484e91ecfa0447c9a2c1326b141e6a68.jpg')
+, ('Bill','Deters','wdeters@gmail.org','555-555-5555','Security Guard','Medium',null)
+, ('Bobby','Johnson','wjohnson@gmail.org','666-666-6666','Science and Research','High','https://st2.depositphotos.com/2931363/6569/i/950/depositphotos_65699901-stock-photo-black-man-keeping-arms-crossed.jpg')
+, ('Sephanie','Jacobs','sj@yahoo.net','777-777-7777','Security Guard','Medium',null)
+;
+
+INSERT INTO project (codename, description, min_clearance, priority, img)
+VALUES
+  ('ACFs -- Research','','High','Medium',null)
+, ('FRPc -- Containment III','','Low','Very High',null)
+, ('FRPr -- Containment I','','Low','Low',null)
+, ('ACFw -- Research','','High','High',null)
+, ('Prepare Food','Cook the burgers','None','Low','https://www.shutterstock.com/image-photo/classic-hamburger-stock-photo-isolated-600nw-2282033179.jpg')
+;
