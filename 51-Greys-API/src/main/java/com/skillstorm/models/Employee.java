83dc6b1f79d977f0f0241efa8e08ecacd22de378
hhjfdkl <nate.koroman@gmail.com>
2024-08-07 03:58:00 -0400

    Added SailPoint account ID to our database and entity model

--- 51-Greys-API/src/main/java/com/skillstorm/models/Employee.java
+++ 51-Greys-API/src/main/java/com/skillstorm/models/Employee.java
@@ -1,6 +1,5 @@
 package com.skillstorm.models;
 
-import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -11,8 +10,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -57,27 +54,22 @@ public class Employee
 	@Column(name = "sp_id")
 	private String spId;
 	
-	public String getSpId() {
-		return spId;
-	}
-
-
-	public void setSpId(String spId) {
-		this.spId = spId;
-	}
-
+	@Column(name = "sp_acct_id")
+	private String spAcctId;
 
 	@ManyToOne
 	@JoinColumn(name="project_id", referencedColumnName = "project_id")
 	@JsonIgnoreProperties({"employees", "minClearance"})
 	private Project project;
 	
+	
+	//Default constructor
 	public Employee()
 	{
 		super();
 	}
 	
-	
+	//Constructor overload for all info that will display on the front
 	public Employee(
 			  int id
 			, String firstName
@@ -104,6 +96,7 @@ public class Employee
 		this.project = project;
 	}
 	
+	//Constructor overload allowing SailPoint User id
 	public Employee(
 			  int id
 			, String firstName
@@ -132,81 +125,99 @@ public class Employee
 		this.project = project;
 	}
 
+	//Constructor overload allowing SailPoint Account id
+	//Account MUST have a user, so we won't make a constructor
+	//for SP account without a SP user
+	public Employee(
+			  int id
+			, String firstName
+			, String lastName
+			, String email
+			, String phoneNumber
+			, String occupation
+			, Clearance clearance
+			, Location location
+			, String img
+			, String spId
+			, String spAcctId
+			, Project project
+			)
+	{
+		super();
+		this.id = id; 
+		this.firstName = firstName; 
+		this.lastName = lastName; 
+		this.email = email; 
+		this.phoneNumber = phoneNumber;
+		this.occupation = occupation;
+		this.clearance = clearance;
+		this.location = location;
+		this.img = img;
+		this.spId = spId;
+		this.spAcctId = spAcctId;
+		this.project = project;
+	}
 
+	
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-
 	public String getLastName() {
 		return lastName;
 	}
-
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-
 
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
-
 	public String getOccupation() {
 		return occupation;
 	}
-
 
 	public void setOccupation(String occupation) {
 		this.occupation = occupation;
 	}
 
-
 	public Clearance getClearance() {
 		return clearance;
 	}
-
 
 	public void setClearance(Clearance clearance) {
 		this.clearance = clearance;
 	}
 
-
 	public Location getLocation() {
 		return location;
 	}
-
 
 	public void setLocation(Location location) {
 		this.location = location;
@@ -216,16 +227,29 @@ public class Employee
 		return project;
 	}
 
-
 	public void setProject(Project project) {
 		this.project = project;
 	}
+	
+	public String getSpId() {
+		return spId;
+	}
 
+	public void setSpId(String spId) {
+		this.spId = spId;
+	}
+
+	public String getSpAcctId() {
+		return spAcctId;
+	}
+
+	public void setSpAcctId(String spAcctId) {
+		this.spAcctId = spAcctId;
+	}
 
 	public String getImg() {
 		return img;
 	}
-
 
 	public void setImg(String img) {
 		this.img = img;
@@ -234,6 +258,9 @@ public class Employee
 
 	
 	
+
+
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
