4f2b0006ea4f598ec59c570c14d4c65516b48cd0
hhjfdkl <nate.koroman@gmail.com>
2024-08-08 09:07:45 -0400

    Committing. Commented out an annotation.

--- 51-Greys-API/src/main/java/com/skillstorm/Application.java
+++ 51-Greys-API/src/main/java/com/skillstorm/Application.java
@@ -2,7 +2,7 @@ package com.skillstorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @SpringBootApplication
 //@EnableWebSecurity
